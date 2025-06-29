15fa7036e46c080580df57aaf83c9d24f9495e5f
deepmind32 <olinishrit@gmail.com>
2025-06-29 22:05:41 +0545

    adds components.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
-import "./utilities.css";
-import "./scrollbar.css";
+import "./stylesheets/index.css";
+import "./stylesheets/utilities.css";
+import "./stylesheets/scrollbar.css";
+import "./stylesheets/components.css";
 
 import App from "./app.jsx";
 
