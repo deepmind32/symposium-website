f7135255624b5354cac7a64a2ffbe54dd1d19a8d
deepmind32 <olinishrit@gmail.com>
2025-06-29 21:59:08 +0545

    adds utility classes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './app.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import "./utilities.css";
+import "./scrollbar.css";
+
+import App from "./app.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
