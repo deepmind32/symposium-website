1d61208264675e8e317e3d7b9fd633d73f51d778
deepmind32 <olinishrit@gmail.com>
2025-06-30 19:03:57 +0545

    adds homepage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,42 @@
+import { MdOpenInNew } from "react-icons/md";
+
+import Button from "../../components/button/button";
+import Shapes from "../../components/shapes/shapes";
 import styles from "./home.module.css";
 
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <section className={styles["hero-section"]}>
+        <Shapes variant="small" color="dark-orange" />
+        <main>
+          <h1>Privacy Preserving Federated Learning</h1>
+          <p>
+            Prifed is HAINet’s initiative to promote{" "}
+            <span className="u-color-dark-orange">
+              privacy-preserving federated learning
+            </span>{" "}
+            in{" "}
+            <span className="u-color-dark-orange u-text-no-wrap">
+              Health AI
+            </span>
+            . We host events, release datasets, and build awareness among
+            policymakers, students, researchers, and medical professionals.
+          </p>
+        </main>
+
+        <div className={styles["hero-button__wrapper"]}>
+          <a
+            href="https://hainet.org/?ref=prifed"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button>
+              Learn More About HAINet <MdOpenInNew />
+            </Button>
+          </a>
+        </div>
+      </section>
+    </>
+  );
 }
