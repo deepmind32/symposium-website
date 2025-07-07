47ef5fc45e05c74bbdec5ef28bcf7f9a8169519d
deepmind32 <olinishrit@gmail.com>
2025-07-07 07:44:46 +0545

    gets page back to github pages

--- src/app.jsx
+++ src/app.jsx
@@ -9,35 +9,33 @@ import SymposiumPage from "./pages/symposium/symposium";
 import styles from "./app.module.css";
 
 const menu_items = [
-  { href: "/", text: "Home" },
-  { href: "/about", text: "About PriFed" },
-  { href: "/datahub", text: "DataHub" },
-  { href: "/symposium", text: "Symposium" },
+	{ href: "/", text: "Home" },
+	{ href: "/about", text: "About PriFed" },
+	{ href: "/datahub", text: "DataHub" },
+	{ href: "/symposium", text: "Symposium" },
 ];
 
 const cta_item = {
-  href: "https://forms.gle/gpUVx16gSf5jsM5x7",
-  text: "Register Now",
+	href: "https://forms.gle/gpUVx16gSf5jsM5x7",
+	text: "Register Now",
 };
 
 export default function App() {
-  return (
-    <HashRouter>
-      <Navbar
-        menu_items={menu_items}
-        cta_item={cta_item}
-        pictorial="/pictorial.png"
-        wordmark="/logo--white.png"
-      />
+	return (
+		<HashRouter>
+			<Navbar
+				menu_items={menu_items}
+				cta_item={cta_item}
+				pictorial="./pictorial.png"
+				wordmark="./logo--white.png"
+			/>
 
-      <div className={styles["main-wrapper"]}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/datahub" element={<DataHubPage />} />
-          <Route path="/symposium" element={<SymposiumPage />} />
-        </Routes>
-      </div>
-    </HashRouter>
-  );
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/about" element={<AboutPage />} />
+				<Route path="/datahub" element={<DataHubPage />} />
+				<Route path="/symposium" element={<SymposiumPage />} />
+			</Routes>
+		</HashRouter>
+	);
 }
