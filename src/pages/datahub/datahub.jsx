7fb507a6e90c49f3f1a8b1ec14066a1da8876dee
deepmind32 <olinishrit@gmail.com>
2025-07-08 08:15:38 +0545

    adds coming soon page in datahub

--- src/pages/datahub/datahub.jsx
+++ src/pages/datahub/datahub.jsx
@@ -1,5 +1,22 @@
+import HeroTitle from "../../components/hero-title/hero-title";
+import Shapes from "../../components/shapes/shapes";
 import styles from "./datahub.module.css";
 
 export default function DataHubPage() {
-  return <h1>Data Hub Page</h1>;
+	return (
+		<>
+			<section className={styles["hero"]}>
+				<Shapes variant="small" color="dark-orange" />
+				<HeroTitle title="PriFed DataHub" />
+				<p>
+					We plan to enable secure, ethical, and inclusive sharing of health
+					datasets through privacy-preserving technologies.
+				</p>
+			</section>
+
+			<section className={styles["datahub__coming-soon"]}>
+				<h1 className="u-gradient-underline">Coming Soon</h1>
+			</section>
+		</>
+	);
 }
